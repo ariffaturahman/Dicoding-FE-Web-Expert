1359167582f46c601900d85375294ee547854dfe
arif_faturahman <arif.afrydo@gmail.com>
2021-06-20 08:47:41 +0700

    Update webpack

--- webpack.common.js
+++ webpack.common.js
@@ -24,8 +24,8 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/,
         use: [
-            'file-loader',
-         ],
+          'file-loader',
+        ],
       },
     ],
   },
