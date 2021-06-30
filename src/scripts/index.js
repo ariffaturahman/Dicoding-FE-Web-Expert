6d553178c56eccb1be0cfeeff2c138f1b6b8ec0b
arif_faturahman <arif.afrydo@gmail.com>
2021-07-01 04:35:27 +0700

    change app-bar menu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,32 +1,19 @@
 import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
+import '../styles/style.css';
 import '../styles/responsive.css';
+import App from './views/app';
 import './data.js';
 
-const menu = document.querySelector('#menu');
-const header = document.querySelector('header');
-const jumbotron = document.querySelector('.jumbotron');
-const main = document.querySelector('main');
-const drawer = document.querySelector('#drawer');
-const title = document.querySelector('.jumbo-title');
-
-menu.addEventListener('click', function(event) {
-  drawer.classList.toggle('open');
-  event.stopPropagation();
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
 });
 
-jumbotron.addEventListener('click', function() {
-  drawer.classList.remove('open');
+window.addEventListener('hashchange', () => {
+  app.renderPage();
 });
 
-header.addEventListener('click', function() {
-  drawer.classList.remove('open');
-});
-
-main.addEventListener('click', function() {
-  drawer.classList.remove('open');
-});
-
-title.addEventListener('click', function() {
-  drawer.classList.remove('open');
+window.addEventListener('load', () => {
+  app.renderPage();
 });
